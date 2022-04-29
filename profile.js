7f90af29f3ba7735ea32d7729ac4f660398ad25e
Ethan Sharp <ethansharpdev@gmail.com>
2022-04-29 11:12:22 -0600

    problem 4 solved

--- profile.js
+++ profile.js
@@ -1,1 +1,16 @@
-console.log('helloworld')
+const colorBtn = document.querySelector('#color');
+const placeBtn = document.querySelector('#place');
+const ritualBtn = document.querySelector('#ritual');
+
+
+colorBtn.addEventListener('click', () => {
+    alert('Black is my favorite color.')
+})
+
+placeBtn.addEventListener('click', () => {
+    alert('Home is my favorite place.')
+})
+
+ritualBtn.addEventListener('click', () => {
+    alert('My favorite ritual is getting a soda from the gas station and coming home to make music.')
+})
