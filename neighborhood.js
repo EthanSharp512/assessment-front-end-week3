eab3871b8babbb0334bc0b34a441f497b35aedfe
Ethan Sharp <ethansharpdev@gmail.com>
2022-04-29 12:55:13 -0600

    final extra credit random button working

--- neighborhood.js
+++ neighborhood.js
@@ -3,10 +3,9 @@ const randResBtn = document.querySelector(".random-button")
 function getRandRes() {
 
     const restaurants = ["Tio's Restaurant", "Cupbob", "The Break"];
-    var rand = restaurants[(Math.random() * myArray.length) | 0];
-    console.log(rand)
+    let eatHere = restaurants[(Math.random() * restaurants.length) | 0];
     document.querySelector('.rand-res').textContent = eatHere;
-
+    console.log(eatHere);
 }
 
 randResBtn.addEventListener('click', getRandRes);
