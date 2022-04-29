88d274b0b6ce7f72cb6f9fdfcff555b64392d558
Ethan Sharp <ethansharpdev@gmail.com>
2022-04-29 10:24:18 -0600

    problem 2 solved

--- about.js
+++ about.js
@@ -6,9 +6,20 @@ function handleSubmit(evt) {
 	evt.preventDefault();
 	
 	console.log('form submit');
+	window.alert('Form has been submitted successfully!')
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+
+const image = document.querySelector('img');
+
+image.addEventListener('mouseover', function(){
+	alert("You are cool like this duck!");
+});
+
+
+// Add an event listener that listens for a mouseover event. When the user mouses over the picture on the page (cat or rubber duck) the page should alert give the user a compliment.
